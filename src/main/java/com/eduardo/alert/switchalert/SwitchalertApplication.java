e9399d213770a3bc78f1b27000536d9af071faef
Eduardo Marquez <oeo28@txstate.edu>
2025-07-10 18:44:56 -0500

    initial spring boot upload

---
+++ src/main/java/com/eduardo/alert/switchalert/SwitchalertApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.alert.switchalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitchalertApplication.class, args);
+	}
+
+}
